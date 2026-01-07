5d5fd2516b4c5c39c34aeef6050a211a38ca3aac
Julles Mitoura <julles.mitoura@celanese.com>
2026-01-06 23:31:45 -0300

    corretions on setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as arq:
 
 setup(
     name='tes_thermo',
-    version='0.1.8',
+    version='0.1.9',
     license='MIT License',
     author='Julles Mitoura, Antonio Freitas and Adriano Mariano',
     author_email='mitoura96@outlook.com',
@@ -21,15 +21,14 @@ setup(
     },
     install_requires=[
         'pandas==2.3.1',
-        'numpy',
+        'numpy==2.4.0',
         'scipy==1.16.0',
         'pyomo==6.9.2',
-        'thermo==0.4.2',
-        'faiss-cpu==1.11.0',
+        'thermo==0.6.0',
+        'chemicals==1.5.0',
+        'faiss-cpu==1.13.2',
         'PyMuPDF==1.26.1',
-        'langchain==0.3.13',
-        'langchain_openai==0.2.14',
-        'langchain_community==0.3.5',
-        'langchain_experimental==0.3.4'
+        'python-dotenv==1.2.1',
+        'openai==2.14.0',
     ],
 )
